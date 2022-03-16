921c24a28b1dfc6672bed8a6b13622a6d059c330
alexander <zeimedee@gmail.com>
2022-03-16 15:51:27 +0000

    testing distributed log with raft set up:rocket:

--- internal/discovery/discovery_test.go
+++ internal/discovery/discovery_test.go
@@ -1,1 +1,40 @@
 package log_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/raft"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+	"github.com/zeimedee/proglog/internal/log"
+)
+
+func TestMultipleNodes(t *testing.T) {
+	var logs []*log.DistributedLog
+	nodeCount := 3
+	ports := dynaport.Get(nodeCount)
+
+	for i := 0; i < nodeCount; i++ {
+		dataDir, err := ioutil.TempDir("", "distributed-lof-test")
+		require.NoError(t, err)
+		defer func(dir string) {
+			_ = os.RemoveAll(dir)
+		}(dataDir)
+
+		ln, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", ports[i]))
+		require.NoError(t, err)
+
+		config := log.Config{}
+		config.Raft.StreamLayer = log.NewStreamLayer(ln, nil, nil)
+		config.Raft.LocalID = raft.ServerID(fmt.Sprintf("%d", i))
+		config.Raft.HeartbeatTimeout = 50 * time.Millisecond
+		config.Raft.ElectionTimeout = 50 * time.Millisecond
+		config.Raft.LeaderLeaseTimeout = 50 * time.Millisecond
+		config.Raft.CommitTimeout = 5 * time.Millisecond
+	}
+}
